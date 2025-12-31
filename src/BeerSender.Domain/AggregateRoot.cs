b32dc9c9288a1af876063882c805368cc07e1fb6
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2025-12-30 20:04:30 -0400

    Initial commit

---
+++ src/BeerSender.Domain/AggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerSender.Domain;
+
+public abstract class AggregateRoot
+{
+    public void Apply(object @event) { }
+}
---
+++ src/BeerSender.Domain/Boxes/Box.cs
@@ -0,0 +1,69 @@
+﻿using System.Numerics;
+
+namespace BeerSender.Domain.Boxes;
+
+public class Box : AggregateRoot
+{
+    public BoxCapacity? Capacity { get; private set; }
+    public ShippingLabel? ShippingLabel { get; private set; }
+
+    public void Apply(BoxCreated @event)
+    {
+        Capacity = @event.Capacity;
+    }
+
+    public void Apply(ShippingLabelAdded @event)
+    {
+        ShippingLabel = @event.Label;
+    }
+}
+
+public record CreateBox(
+    Guid BoxId, 
+    int DesiredNumbersOfSpots
+);
+
+public record BoxCreated(BoxCapacity Capacity);
+public record ShippingLabelAdded(
+    ShippingLabel Label
+);
+
+public record ShippingLabelFailedToAdd(ShippingLabelFailedToAdd.FailReason Reason)
+{
+    public enum FailReason
+    {
+        TrackingCodeInvalid
+    }
+}
+
+public record ShippingLabel(Carrier Carrier, string TrackingCode)
+{
+    public bool IsValid()
+        => Carrier switch
+        {
+            Carrier.UPS => TrackingCode.StartsWith("ABC"),
+            Carrier.FedEx => TrackingCode.StartsWith("DEF"),
+            Carrier.BPost => TrackingCode.StartsWith("GHI"),
+            _ => throw new ArgumentOutOfRangeException(nameof(Carrier), Carrier, null),
+        };
+}
+
+public record BoxCapacity(int NumberOfSpots)
+{
+    public static BoxCapacity Create(int desiredNumberOfSpots)
+    {
+        return desiredNumberOfSpots switch
+        {
+            <= 6 => new BoxCapacity(6),
+            <= 12 => new BoxCapacity(12),
+            _ => new BoxCapacity(24)
+        };
+    }
+}
+
+public enum Carrier
+{
+    UPS,
+    FedEx,
+    BPost
+}
