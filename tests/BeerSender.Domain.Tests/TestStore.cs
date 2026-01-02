cdc45a6aa68a4f96e1b8750ed645b9c931e12c4d
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2026-01-02 13:23:38 -0400

    (feat): Added base testing class

---
+++ tests/BeerSender.Domain.Tests/TestStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerSender.Domain.Tests
+{
+    public class TestStore : IEventStore
+    {
+        public List<StoredEvent> previousEvents = [];
+
+        public List<StoredEvent> newEvents = [];
+        public void AppendEvent(StoredEvent @event)
+        {
+            newEvents.Add(@event);
+        }
+
+        public IEnumerable<StoredEvent> GetEvents(Guid aggregateId)
+        {
+            return previousEvents
+                .Where(e => e.AggregateId == aggregateId)
+                .ToList();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
