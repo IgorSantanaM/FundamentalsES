c17e722aa07d173bad9f4f12d5c54ce6fd8dea5a
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2026-01-04 21:27:04 -0400

    (feat): Added API

---
+++ src/BeerSender.EventStore/EventStoreExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BeerSender.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerSender.EventStore
+{
+    public static class EventStoreExtensions
+    {
+        public static void RegisterEventStore(this IServiceCollection services)
+        {
+            services.AddSingleton<EventStoreConnectionFactory>();
+
+            services.AddScoped<IEventStore, EventStore>();
+        }
+    }
+}
--- src/BeerSender.Web/Controllers/BoxControllers.cs
+++ src/BeerSender.Web/Controllers/BoxControllers.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BeerSender.Domain;
+using BeerSender.Domain.Boxes.Commands;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BeerSender.Web.Controllers;
 
 [ApiController]
 [Route("api/command/[controller]")]
-public class BoxControllers : ControllerBase
+public class BoxControllers(CommandRouter router) : ControllerBase
 {
+    [HttpPost]
+    [Route("create")]
+    public IActionResult CreateBox([FromBody] CreateBox command)
+    {
+        router.HandleCommand(command);
+        return Accepted();
+    }
 }
